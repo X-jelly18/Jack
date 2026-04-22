4b32bf0fba2e4b19ab209dc10f1e350d7c6275d2
X-jelly18 <elitegamer128k@gmail.com>
2026-04-22 12:08:57 +0200

    Update and rename proxy.ts to api/proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "http://south.ayanakojivps.shop" +
+    "http://sela.ayanakojivps.shop" +
     url.pathname +
     url.search;
 
